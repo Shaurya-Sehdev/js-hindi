ec3419f98df66b802df1528e40bdb11e9dacf767
Shaurya-Sehdev <105313436+Shaurya-Sehdev@users.noreply.github.com>
2024-05-29 18:34:30 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -29,3 +29,51 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ************ OPERATIONS *********//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+
+// console.log(2*2);
+// console.log(2**3); raise to power
+
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh" //1 gap before first letter given
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log( (3 + 4) * 5 % 3); // means 35%3 = 2
+//All these maybe imp for Exam Point OF View but real life is more practical and code should be simple and easy to read and ye sabh karate bhi nhi zada but should know. If want to llearn more indepth mai reasons then read documentation at EMEC else memorise the basics ones used//
+
+console.log(+true); //1
+console.log(+""); //0
+
+// console.log(true+); 
+// console.log(""+); ERROR
+
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2 //not a smart way code should be readable.
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+//prefix and postfix operator aata hai obv
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
