505af1b47e36694241c647c73f7ce89af7bd0aa8
Shaurya-Sehdev <105313436+Shaurya-Sehdev@users.noreply.github.com>
2024-05-30 19:36:08 +0000

    Dates_first

---
+++ 01_Basics/Dates.js
@@ -0,0 +1,11 @@
+ // in future Date will be treated like object like Math and will have its own functions, a proposal made called Temporal associates with date The  Temporal.Instant ( . . . ) returns current system exact time etc,
+//ALL THIS IS IN FUTURE RIGHT NOW WE WILL HAVE TO STUDY THE TRADITIONAL METHO//
+
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
