9aa27b9b09596ef52d0861cc13b7477ff7528b77
Shaurya-Sehdev <105313436+Shaurya-Sehdev@users.noreply.github.com>
2024-08-06 01:43:13 +0000

    PROMISES

---
+++ 09_Advance_One/promises.js
@@ -0,0 +1,118 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+/*Promise Creation:
+
+A new Promise promiseOne is created. The executor function (the function passed to the Promise constructor) starts an asynchronous task using setTimeout.
+The setTimeout function waits for 1 second (1000 milliseconds) and then logs "Async task is complete" to the console and calls resolve() to fulfill the promise.
+Promise Consumption:
+
+The then method is used to attach a callback function to promiseOne. This callback function will be executed when the promise is resolved.
+When the promise is resolved, "Promise consumed" is logged to the console.*/
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json() ISS LINE MAI AWAIT DUBARA CAUSE CONVERSION OF DATA TO OBJECT ALSO TAKES TIME AND MUST BE AWAITED TO RETURN SOMETHING.
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
+//Sure! Promise.all is a method that allows you to run multiple promises in parallel and wait for all of them to complete.
+
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve) => setTimeout(resolve, 100, 'foo'));
+const promise3 = 42;
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values); // [3, 'foo', 42]
+});
