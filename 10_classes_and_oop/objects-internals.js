5751d82d61ce696d34b053f8963c894be31f6b01
Shaurya-Sehdev <105313436+Shaurya-Sehdev@users.noreply.github.com>
2024-08-08 02:16:36 +0000

    object-internal-MATH-PI

---
+++ 10_classes_and_oop/objects-internals.js
@@ -0,0 +1,35 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")// math ek object hai in js defined jiske pi ke flags dekhne ke liye ye method hai
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})//can change the default flags set by js aise
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
+//.entries method makes non iterable objects iterable so that uske har element pe vo iterate kare
