905c4444e646a4d24cd77ee6f21e48aff98e12a2
Shaurya-Sehdev <105313436+Shaurya-Sehdev@users.noreply.github.com>
2024-08-08 02:50:43 +0000

    getter_setter_intro

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){// ***this.email has become the value now so aaege to upar vale constructor se hi but then furthur set aise and returned to outside by get function***
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "abc")
+console.log(hitesh.email);
